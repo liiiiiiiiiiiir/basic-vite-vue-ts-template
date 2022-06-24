339f0253fe33009bdc5129ea5f0f150d6ecd9c2e
Zero-Misaka <1171387014@qq.com>
2022-06-24 11:17:07 +0800

    [style]: yarn lint

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,9 +1,3 @@
-<template>
-  <div class="index-page">
-    <h1>Hello {{ trans }}</h1>
-  </div>
-</template>
-
 <script lang="ts" setup>
 import { computed } from "vue";
 import { useIntervalFn } from "@vueuse/core";
@@ -14,12 +8,20 @@ const trans = computed(() => indexStore.trans);
 
 useIntervalFn(() => {
   indexStore.toggleLang();
-}, 1000);
+}, 1500);
 </script>
+
+<template>
+  <div class="index-page">
+    <h1>Hello {{ trans }}</h1>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 h1 {
   text-align: center;
   padding: 20px 0;
+  height: 40px;
+  line-height: 40px;
 }
 </style>
